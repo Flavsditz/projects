9390877315264f0954263b5e23bd3771ed731318
Flavio Diez <flaviofdiez@gmail.com>
2013-09-24 18:48:26 -0300

    added python script

---
+++ sorting/sorting.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+############################
+#
+#  Author: Flavio Diez
+# Version: 0.1
+#
+#    Info: This program implements different sorting algorithms in order to test the times of sorting of each implementation.
+#          Each function returns its run time.
+#
+############################
+
+
+
+def quicksort():
+    pass
+
+def bubblesort();
+    pass
