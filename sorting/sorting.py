ef2d9286b6b40ea161a5f7881d25ba146eae57f7
Flavio Diez <flaviofdiez@gmail.com>
2013-09-27 16:32:23 -0300

    Corrected quicksorted and started implementation of mergesort(bugged)

--- sorting/sorting.py
+++ sorting/sorting.py
@@ -36,13 +36,46 @@ def quickSort(lis):
 
     for x in lis:
         if x <= pivot:
-            less.append(less)
+            less.append(x)
         else:
-            greater.append
+            greater.append(x)
+     
     return quickSort(less) + [pivot] + quickSort(greater)
 
-def mergeSort():
-    pass
+def mergeSort(lis):
+    """Mergesort is a algorithm which divides recursively a list until it is of length 1. Then it comes back the recursion stack by taking every two lists and organizing them into one. This is done until the list becomes whole again but in the right order.
+
+    :param lis: A list of unordered values that the algorithm has to sort.
+    :type lis: list of floats.
+    :returns:  list -- organized list.
+
+    """
+    left = []
+    right = []
+    
+    # Recursively break the list
+    if len(lis) <= 1:
+        return lis
+    else:
+        left = mergeSort(lis[:len(lis)/2])
+        right = mergeSort(lis[len(lis)/2:])
+
+    print "Left -"+str(left)
+    print "Right -"+str(right)
+
+    #Start to organize the list:
+    ans = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        # Add to final list
+        if left[i] < right[j]:
+            ans.append(left.pop(0))
+            i += 1
+        else:
+            ans.append(right.pop(0))
+            j += 1
+
+    return ans
 
 def inplaceMergeSort():
     pass
@@ -99,15 +132,31 @@ def gnomeSort():
     pass
 
 def main(size):
-    l = []
+    randList = []
     for i in range(size):
-        l.append(random.random())
+        randList.append(random.randint(1,100))
+
+    # Print array
+    print "List:  "+str(randList)
+    print '\n'
 
     # Quicksort Algorithm
+    print "Quicksort Algorithm\n-------------------"
+    l = randList[:]
     toc = time.time()
-    quickSort(l)
+    print quickSort(l)
     tic = time.time()
-    print 'Quicksort - ' + str(tic - toc)
+    print 'Time - ' + str(tic - toc)
+
+    print '\n\n'
+    
+    # Mergesort Algorithm
+    print "Mergesort Algorithm\n-------------------"
+    l = randList[:]
+    toc = time.time()
+    print mergeSort(l)
+    tic = time.time()
+    print 'Time - ' + str(tic - toc)
 
 if __name__=="__main__":
     main(int(sys.argv[1]))
